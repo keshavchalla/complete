442c156903cbdc593ca031d0877910ddbe3dd443
keshav <challa.keshav@gmail.com>
2016-11-04 11:41:58 -0700

    added customer detail service

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,10 +1,14 @@
 package hello;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import hello.model.City;
+import hello.model.CustomerDetails;
 import hello.model.WeatherObservation;
 import hello.service.WeatherService;
 
@@ -35,6 +39,27 @@ public class HelloController {
     	city.setAlias("WestChester");
     	wo.setCity(city);
     	return wo;
+    }
+    
+    @RequestMapping("/customerDetails")
+    public List<CustomerDetails> getCustomerDetails() {
+    	List<CustomerDetails> cdList = new ArrayList<CustomerDetails>();
+    	CustomerDetails cd1 = new CustomerDetails();
+    	cd1.setAccountNumber("8919100010000017");
+    	cd1.setTnType("UNKNOWN");
+    	
+    	CustomerDetails cd2 = new CustomerDetails();
+    	cd2.setAccountNumber("8210100200055975");
+    	cd2.setTnType("UNKNOWN");
+    	
+    	CustomerDetails cd3 = new CustomerDetails();
+    	cd3.setAccountNumber("8919100011045425");
+    	cd3.setTnType("UNKNOWN");
+    	
+    	cdList.add(cd1);
+    	cdList.add(cd2);
+    	cdList.add(cd3);
+    	return cdList;
     }
     
 }
---
+++ src/main/java/hello/model/CustomerDetails.java
@@ -0,0 +1,20 @@
+package hello.model;
+
+public class CustomerDetails {
+
+	private String accountNumber;
+	private String tnType;
+	
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public String getTnType() {
+		return tnType;
+	}
+	public void setTnType(String tnType) {
+		this.tnType = tnType;
+	}
+}
